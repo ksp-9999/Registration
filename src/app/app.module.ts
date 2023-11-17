1b2e7ddee4a7866afd04ce141b09d7d01824e29a
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2023-11-17 19:48:58 +0530

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './userpages/new-user/new-user.component';
 import { UserListComponent } from './userpages/user-list/user-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
+
+const routes:Routes=[
+  {
+    path:'newuser',
+    component:NewUserComponent
+  },
+  {
+    path:'userslist',
+    component:UserListComponent
+  },
+  {
+    path:'**',
+    component:NewUserComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +31,11 @@ import { UserListComponent } from './userpages/user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/userpages/new-user/new-user.component.ts
+++ src/app/userpages/new-user/new-user.component.ts
@@ -1,10 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+CommonModule
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.css']
 })
 export class NewUserComponent {
+userForm:FormGroup;
+constructor(){
+  this.userForm= new FormGroup({})
+  this.createForm()
+}
+createForm(){
+  this.userForm=new FormGroup({
+    firstname: new FormControl('',[Validators.required]),
+    lastname:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required]),
+    phone:new FormControl('',[Validators.required]),
+    status:new FormControl('',[Validators.required]),
+    dob:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.required]),
+    address:new FormGroup({
+    country:new FormControl('',[Validators.required]),
+      state: new FormControl('',[Validators.required]),
+      city:new FormControl('',[Validators.required])
+    })
+  })
+}
 
+onsave(){
+      this.userForm.markAllAsTouched();
+    }
 }
